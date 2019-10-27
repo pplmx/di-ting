54f6a3857b784a05e94ec1bbe8f9fcda50eeb955
mystic <215104920@qq.com>
2019-10-27 15:19:21 +0800

    a little bit hard

--- parser.py
+++ parser.py
@@ -3,5 +3,21 @@
 
 from pyhanlp import *
 
+analyzer = PerceptronLexicalAnalyzer(
+    'C:/Users/purpl/.virtualenvs/BuildRoleRelationship4Novel/Lib/site-packages/pyhanlp/static/data/model/perceptron/large/cws.bin',
+    HanLP.Config.PerceptronPOSModelPath,
+    HanLP.Config.PerceptronNERModelPath)
+tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
+
+
+def prepare(file):
+    with open(file, encoding='UTF-8') as f:
+        for line in f.readlines():
+            if not line.strip():
+                continue
+            print(tokenizer.analyze(line).translateLabels())
+
+
 if __name__ == '__main__':
-    print('hello world')
+    # prepare('惟我独仙.txt')
+    print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
