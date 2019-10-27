2adcbde20acefdfb9283cb431b1555b5b8e04773
mystic <215104920@qq.com>
2019-10-27 21:10:44 +0800

    a little bit hard

--- parser.py
+++ parser.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyhanlp import *
+import stanfordnlp
+# from pyhanlp import *
 
-analyzer = PerceptronLexicalAnalyzer(
-    'C:/Users/purpl/.virtualenvs/BuildRoleRelationship4Novel/Lib/site-packages/pyhanlp/static/data/model/perceptron/large/cws.bin',
-    HanLP.Config.PerceptronPOSModelPath,
-    HanLP.Config.PerceptronNERModelPath)
-tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
-
-
-def prepare(file):
-    with open(file, encoding='UTF-8') as f:
-        for line in f.readlines():
-            if not line.strip():
-                continue
-            print(tokenizer.analyze(line).translateLabels())
+# analyzer = PerceptronLexicalAnalyzer(
+#     'C:/Users/purpl/.virtualenvs/BuildRoleRelationship4Novel/Lib/site-packages/pyhanlp/static/data/model/perceptron/large/cws.bin',
+#     HanLP.Config.PerceptronPOSModelPath,
+#     HanLP.Config.PerceptronNERModelPath)
+# tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
+#
+#
+# def prepare(file):
+#     with open(file, encoding='UTF-8') as f:
+#         for line in f.readlines():
+#             if not line.strip():
+#                 continue
+#             print(tokenizer.analyze(line).translateLabels())
 
 
 if __name__ == '__main__':
     # prepare('惟我独仙.txt')
-    print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
+    # print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
+    stanfordnlp.download('en', force=True)
+    # stanfordnlp.download('zh', force=True)
