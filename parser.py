d09fd61f7e9bcb667c63be7ce9c2c9f56f501ebc
mystic <215104920@qq.com>
2019-10-29 21:12:43 +0800

    a little bit hard

--- parser.py
+++ parser.py
@@ -6,26 +6,25 @@
     哈斯图
 """
 
-import stanfordnlp
-# from pyhanlp import *
+from pyhanlp import *
 
-# analyzer = PerceptronLexicalAnalyzer(
-#     'C:/Users/purpl/.virtualenvs/BuildRoleRelationship4Novel/Lib/site-packages/pyhanlp/static/data/model/perceptron/large/cws.bin',
-#     HanLP.Config.PerceptronPOSModelPath,
-#     HanLP.Config.PerceptronNERModelPath)
-# tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
-#
-#
-# def prepare(file):
-#     with open(file, encoding='UTF-8') as f:
-#         for line in f.readlines():
-#             if not line.strip():
-#                 continue
-#             print(tokenizer.analyze(line).translateLabels())
+analyzer = PerceptronLexicalAnalyzer()
+tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
+
+
+def prepare(file):
+    with open(file, encoding='UTF-8') as f:
+        i = 0
+        for line in f.readlines():
+            i += 1
+            # filter the empty line
+            line = line.strip()
+            if line:
+                if i < 10:
+                    print(i, line)
+                    print(tokenizer.analyze(line).translateLabels())
 
 
 if __name__ == '__main__':
-    # prepare('惟我独仙.txt')
+    prepare('惟我独仙.txt')
     # print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
-    stanfordnlp.download('en', force=True)
-    # stanfordnlp.download('zh', force=True)
