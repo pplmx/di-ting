448e85cf272ad66432bc54ffae04926f430cfb23
mystic <215104920@qq.com>
2019-11-06 21:05:40 +0800

    a little bit forward

--- parser.py
+++ parser.py
@@ -17,24 +17,25 @@ crf_seg = HanLP.newSegment('crf')
 
 def read_novel_hanlp(file):
     all_part_speech_list = []
+    role_list = []
     with open(file, encoding='UTF-8') as f:
         i = 0
         for line in f:
             i += 1
             # filter the empty line
             line = line.strip()
+            line = line.replace(' ', '')
             if line:
-                if i < 10:
-                    print(i, line)
-                    # transfer to python string
-                    # line_part_speech_list = crf_seg.seg(line).__str__()
-                    line_part_speech_list = tokenizer.analyze(line).translateLabels().__str__().split(' ')
-                    # convert to tuple list
-                    line_part_speech_list = [(j.split('/')[0], j.split('/')[1]) for j in line_part_speech_list]
-                    # filter the word whose length is less than 2
-                    line_part_speech_list = list(filter(lambda x: len(x[0]) > 1, line_part_speech_list))
-                    all_part_speech_list += line_part_speech_list
-        return all_part_speech_list
+                # transfer to python string
+                # line_part_speech_list = crf_seg.seg(line).__str__()
+                line_part_speech_list = tokenizer.analyze(line).translateLabels().__str__().split(' ')
+                # convert to tuple list
+                line_part_speech_list = [(j.split('/')[0], j.split('/')[1]) for j in line_part_speech_list]
+                # filter the word whose length is less than 2
+                line_part_speech_list = list(filter(lambda x: len(x[0]) > 1, line_part_speech_list))
+                role_list += list(filter(lambda x: len(x[1]) == '人名', line_part_speech_list))
+                all_part_speech_list += line_part_speech_list
+        return all_part_speech_list, role_list
 
 
 def read_novel_jieba(file):
@@ -54,8 +55,8 @@ def read_novel_jieba(file):
 
 
 if __name__ == '__main__':
-    ll = read_novel_hanlp('惟我独仙.txt')
+    all_list, roles = read_novel_hanlp('惟我独仙.txt')
     # print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
     # ll = read_novel_jieba('惟我独仙.txt')
-    ll = Counter(ll)
-    print(ll)
+    roles = Counter(roles)
+    print(roles)
