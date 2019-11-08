7fb9744b4e718ccc66fb997c8112970d4ba39f20
mystic <215104920@qq.com>
2019-11-08 21:42:46 +0800

    a little bit forward

--- parser.py
+++ parser.py
@@ -12,6 +12,8 @@ import jieba.posseg
 from pyhanlp import *
 
 tokenizer = JClass("com.hankcs.hanlp.tokenizer.NLPTokenizer")
+stop_word_dict = JClass("com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary")
+# viterbi, dat, crf, perceptron, nshort
 crf_seg = HanLP.newSegment('crf')
 
 
@@ -26,14 +28,22 @@ def read_novel_hanlp(file):
             line = line.strip()
             line = line.replace(' ', '')
             if line:
-                # transfer to python string
-                # line_part_speech_list = crf_seg.seg(line).__str__()
-                line_part_speech_list = tokenizer.analyze(line).translateLabels().__str__().split(' ')
-                # convert to tuple list
-                line_part_speech_list = [(j.split('/')[0], j.split('/')[1]) for j in line_part_speech_list]
+                # ====== CRF ======
+                line_part_speech_list = crf_seg.seg(line)
+                # enable stopwords
+                stop_word_dict.apply(line_part_speech_list)
+                line_part_speech_list = [(str(j.word), str(j.nature)) for j in line_part_speech_list]
+                # print(line_part_speech_list)
+
+                # ====== NLP tokenizer ======
+                # line_part_speech_list = tokenizer.analyze(line).translateLabels().__str__().split(' ')
+                # # convert to tuple list
+                # line_part_speech_list = [(j.split('/')[0], j.split('/')[1]) for j in line_part_speech_list]
+                # print(line_part_speech_list)
+
                 # filter the word whose length is less than 2
                 line_part_speech_list = list(filter(lambda x: len(x[0]) > 1, line_part_speech_list))
-                role_list += list(filter(lambda x: x[1] == '人名', line_part_speech_list))
+                # role_list += list(filter(lambda x: x[1] == '人名', line_part_speech_list))
                 all_part_speech_list += line_part_speech_list
         return all_part_speech_list, role_list
 
@@ -58,5 +68,5 @@ if __name__ == '__main__':
     all_list, roles = read_novel_hanlp('惟我独仙.txt')
     # print(tokenizer.analyze('冥英王楞楞的道：“他，他真的放过了你。  ”'))
     # ll = read_novel_jieba('惟我独仙.txt')
-    roles = Counter(roles)
-    print(roles)
+    all_list = Counter(all_list)
+    print(all_list)
