b7d76b2f2b58fa1dd107a4aaf10aabbee5f931cd
mystic <215104920@qq.com>
2020-09-04 17:00:37 +0800

    upgrade stanza

--- learn_stanza.py
+++ learn_stanza.py
@@ -6,8 +6,10 @@ if __name__ == '__main__':
     # https://github.com/stanfordnlp/stanza/blob/master/demo/Stanza_Beginners_Guide.ipynb
     # Note that you can use verbose=False to turn off all printed messages
     print("Downloading Chinese model...")
-    stanza.download('zh', verbose=False)
+    stanza.download('zh', verbose=True, resources_url='stanford')
 
-    # Build a Chinese pipeline, with customized processor list and no logging, and force it to use CPU
+    # Build a Chinese pipeline, with customized processor list and no logging,
+    # and force it to use CPU
     print("Building a Chinese pipeline...")
-    zh_nlp = stanza.Pipeline('zh', processors='tokenize,lemma,pos,depparse', verbose=False, use_gpu=False)
+    zh_nlp = stanza.Pipeline('zh', processors='tokenize,lemma,pos,depparse',
+                             verbose=True, use_gpu=False)
